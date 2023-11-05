a1cd389b5b6e1be184935fea11974837db964a0b
Awesonel <sidorenivan@mail.ru>
2023-11-05 18:59:56 +0300

    The methods are implemented. A directory with files is created.

--- src/main/java/ru/spbu/apcyb/svp/tasks/dirscan/Directory.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/dirscan/Directory.java
@@ -1,17 +1,28 @@
 package ru.spbu.apcyb.svp.tasks.dirscan;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Directory Class.
+ */
 public class Directory {
 
   private final Path dirPath;
-  private List<Directory> subDirectories;
-  private List<File> files;
+  private final List<Directory> subDirectories = new ArrayList<>();
+  private final List<File> files = new ArrayList<>();
 
+  /**
+   * Creates a directory object at the given path.
+   *
+   * @param path directory path as a string
+   */
   public Directory(String path) {
 
     try {
@@ -27,6 +38,11 @@ public class Directory {
 
   }
 
+  /**
+   * Creates a directory object at the given path.
+   *
+   * @param path directory path
+   */
   public Directory(Path path) {
     if (!Files.isDirectory(path)) {
       throw new IllegalArgumentException("Provided path is not a directory.");
@@ -36,7 +52,23 @@ public class Directory {
   }
 
   private void readContent() {
-    // TODO: 03.11.2023 считать директорию, заполнить списки файлов и директорий
+    try (DirectoryStream<Path> content = Files.newDirectoryStream(dirPath)) {
+
+      for (Path subPath : content) {
+        if (Files.isDirectory(subPath)) {
+          subDirectories.add(new Directory(subPath));
+        } else {
+          files.add(subPath.toFile());
+        }
+      }
+
+    } catch (IOException ex) {
+      throw new RuntimeException(ex.getMessage());
+    }
+  }
+
+  public Path getDirPath() {
+    return dirPath;
   }
 
   public List<Directory> getSubDirectories() {
--- src/main/java/ru/spbu/apcyb/svp/tasks/dirscan/DirectoryScannerMain.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/dirscan/DirectoryScannerMain.java
@@ -1,16 +1,14 @@
 package ru.spbu.apcyb.svp.tasks.dirscan;
 
+/**
+ * Main class for DirectoryScanner.
+ */
 public class DirectoryScannerMain {
 
   public static void main(String[] args) {
-    String pathToScan = "Path";
-    String pathToSave = "Another path";
-    scan(pathToScan, pathToSave);
-  }
-
-  private static void scan(String pathToScan, String pathToSave) {
-    // TODO: 03.11.2023  считать содержимое директории pathToScan и сохранить это в файл,
-    //  который положить в директорию pathToSave
+    DirectoryScanner.scanToFile(
+        "src/test/resources/directory_scan_test/test_directory",
+        "src/test/resources/directory_scan_test/result_dir");
   }
 
 }
