0e2ec27e08715486d6f9e44609e00f823540f665
Awesone <sidorenivan@mail.ru>
2023-11-21 00:37:24 +0300

    added test class structure

---
+++ src/test/java/ru/spbu/apcyb/svp/tasks/multithread/TangentsCalculatorTest.java
@@ -0,0 +1,20 @@
+package ru.spbu.apcyb.svp.tasks.multithread;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class TangentsCalculatorTest {
+
+  @Test
+  void simpleTest() {
+    // TODO: 21.11.2023
+    Assertions.assertTrue(true);
+  }
+
+  @Test
+  void perfomanceTest() {
+    // TODO: 21.11.2023
+    Assertions.assertTrue(true);
+  }
+
+}
